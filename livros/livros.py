56a679f4670edbde61eb519dc1fd6ec14b0d3fea
Arthur Otero <arthurotero2014@gmail.com>
2021-11-21 20:38:17 -0300

    update5

--- livros/livros.py
+++ livros/livros.py
@@ -15,7 +15,7 @@ app = Flask(__name__)
 
 # Cadastrar Livros
 @app.route('/livros/cadastrar', methods=['GET', 'POST'])
-def cadastrar():
+def cadastrar_livro():
     if request.method == 'POST':
 
         nome = request.form['nome']
@@ -75,12 +75,29 @@ def excluir(id=None):
 
 @app.route('/', methods=['GET'])
 def inicio():
-    return render_template('inicio.html')
+    conn = None
+    try:
+
+        conn = sqlite3.connect('db-usuario.db')
+        sql = '''SELECT * FROM usuario'''
+
+        cur = conn.cursor()
+
+        cur.execute(sql)
+
+        registros = cur.fetchall()
+
+        return render_template('Inicio.html', regs=registros)
+
+    except Error as e:
+        print(e)
+    finally:
+        conn.close()
 
 
 # Editar Livros
 @app.route('/livros/editar/<int:id>', methods=['POST'])
-def editar(id=None):
+def editar_livro(id=None):
     if id is None:
         return jsonify({'mensagem': 'Valor invalido'})
     else:
@@ -134,7 +151,75 @@ def listar():
         conn.close()
 
 
-# Pagina erro
+# Cadastros de Usuario
+#######################################################################################################
+@app.route('/usuario/cadastrar', methods=['GET', 'POST'])
+def cadastrar():
+    if request.method == 'POST':
+
+        cpf = request.form['cpf']
+        nome = request.form['nome']
+        telefone = request.form['telefone']
+        email = request.form['email']
+        senha = request.form['senha']
+
+        if cpf and nome and telefone and email and senha:
+            registro = (cpf, nome, telefone, email, senha)
+            conn = None
+            try:
+
+                conn = sqlite3.connect('db-usuario.db')
+                # todo validar login existente
+
+                sql = ''' INSERT INTO usuario(cpf, nome, telefone, email, senha)
+                              VALUES(?,?,?,?,?) '''
+
+                cur = conn.cursor()
+
+                cur.execute(sql, registro)
+
+                conn.commit()
+
+                mensagem = 'Sucesso - cadastrado'
+
+            except Error as e:
+                print(e)
+            finally:
+                conn.close()
+
+    return render_template('cadastrar_usuario.html')
+
+
+# # EDITAR USUARIO
+@app.route('/usuario/editar/<int:login>', methods=['POST'])
+def editar(login=None):
+    if login is None:
+        return jsonify({'mensagem': 'Valor invalido'})
+    else:
+        senha = request.form['senha']
+
+        if senha:
+            registro = (login, senha, login)
+            conn = None
+            try:
+                conn = sqlite3.connect('db-usuario.db')
+                sql = '''UPDATE usuario set cpf = ?, nome = ?, telefone = ?, email = ?, senha = ? WHERE
+                cpf = ? '''
+
+                cur = conn.cursor()
+
+                cur.execute(sql, registro)
+
+                conn.commit()
+
+                return jsonify({'mensagem': 'Senha mudada'})
+
+            except Error as e:
+                return jsonify({'mensagem': e})
+            finally:
+                conn.close()
+
+
 @app.errorhandler(404)
 def pagina_nao_encontrada(e):
     return render_template('404.html'), 404
